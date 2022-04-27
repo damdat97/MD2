abca4023923f366b8d35d11b032610b24c8a37f8
damdat97 <dam.dat97@gmail.com>
2022-04-27 21:58:14 +0700

    Validate Account

---
+++ TH/src/string_regex/validate_account/AccountExample.java
@@ -0,0 +1,23 @@
+package string_regex.validate_account;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class AccountExample {
+    /*Khong chua cac ky tu dac biet
+    Phai dai hon 6 ky tu
+    Khong chua cac ky tu viet hoa
+    cho phep dau gach duoi (_)
+     */
+
+    public static final String ACCOUNT_REGEX = "^[_a-z0-9]{6,}$";
+
+    public AccountExample() {
+    }
+
+    public boolean validate(String regex) {
+        Pattern pattern = Pattern.compile(ACCOUNT_REGEX);
+        Matcher matcher = pattern.matcher(regex);
+        return matcher.matches();
+    }
+}
